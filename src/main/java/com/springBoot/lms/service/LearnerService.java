c8650ab33b0eb5e34421458412a4f55e0bb81118
DhanushRaj2004 <2k21aids11@kiot.ac.in>
2025-05-29 17:41:24 +0530

    LMS-SpringBoot

--- src/main/java/com/springBoot/lms/service/LearnerService.java
+++ src/main/java/com/springBoot/lms/service/LearnerService.java
@@ -36,8 +36,7 @@ public class LearnerService {
 
         return learnerRepository.save(learner);
     }
-
-    //uyfkutdddddddddddd
+    
     public List<Learner> getAll() {
          return learnerRepository.findAll();
     }
